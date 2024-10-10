a674e7acff96c2910260a83877d407fcb7e6e6f9
julian.l47 <julian.l47@P-0G-XXXXX-0090>
2024-10-10 14:29:35 +0200

    Projektdateien hinzufügen.

---
+++ 1_Elementare_Syntax/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Elementare_Syntax
+{
+    internal class Class1
+    {
+    }
+}
